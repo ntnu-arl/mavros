13e44bf13eb8b81708341d4d545de8a84c91edfa
sedith <martin.jacquet@posteo.net>
2025-02-06 18:08:17 +0100

    extend debug message

--- mavros_extras/src/plugins/cbf_debug.cpp
+++ mavros_extras/src/plugins/cbf_debug.cpp
@@ -54,13 +54,18 @@ private:
   void handle_cbf_debug(const mavlink::mavlink_message_t* msg, mavlink::common::msg::CBF_DEBUG& cbf_debug)
   {
     _cbf_debug.header.stamp = m_uas->synchronise_stamp(cbf_debug.time_usec);
+    _cbf_debug.qp_fail = cbf_debug.qp_fail;
     _cbf_debug.h = cbf_debug.h;
+    _cbf_debug.h1 = cbf_debug.h1;
+    _cbf_debug.h2 = cbf_debug.h2;
     _cbf_debug.input.x = cbf_debug.input[0];
     _cbf_debug.input.y = cbf_debug.input[1];
     _cbf_debug.input.z = cbf_debug.input[2];
     _cbf_debug.output.x = cbf_debug.output[0];
     _cbf_debug.output.y = cbf_debug.output[1];
     _cbf_debug.output.z = cbf_debug.output[2];
+    _cbf_debug.slack.x = cbf_debug.slack[0];
+    _cbf_debug.slack.y = cbf_debug.slack[1];
 
     cbf_debug_pub.publish(_cbf_debug);
   }
